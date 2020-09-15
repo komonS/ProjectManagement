2282c492467e709e330af54b87e6fcfc15164d58
komon <silwerbas@gmaill.com>
2020-09-15 16:38:24 +0700

    fix error can not using ngModel by add FormsModule to ngModule in app.module

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, Routes, RouterModule } from '@angular/router';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router) { }
+  username: string = 'test';
+  password: string = '';
+  setUsername(name) {
+    this.username = name
+  }
   ngOnInit(): void {
+  }
+  onLogin() {
+    /*
+    localStorage.userID = this.username
+    this.router.navigate(['']);
+    */
+    alert(this.username)
   }
 
 }
